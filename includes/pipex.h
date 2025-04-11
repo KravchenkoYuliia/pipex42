dc27f72e0aa02d24f592a724aa6bd6e3c5968c22
Yuliia Kravchenko <yukravch@made-f0Br3s1.clusters.42paris.fr>
2025-04-11 14:01:17 +0200

    Training to use the new functions

--- includes/pipex.h
+++ includes/pipex.h
@@ -6,7 +6,7 @@
 /*   By: yukravch <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/09 19:19:12 by yukravch          #+#    #+#             */
-/*   Updated: 2025/04/09 19:45:54 by yukravch         ###   ########.fr       */
+/*   Updated: 2025/04/11 11:12:47 by yukravch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,9 @@
 #include "get_next_line.h"
 
 #include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <fcntl.h>
+
 
 #endif
--- sources/main.c
+++ sources/main.c
@@ -6,7 +6,7 @@
 /*   By: yukravch <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/09 19:18:14 by yukravch          #+#    #+#             */
-/*   Updated: 2025/04/10 21:13:09 by yukravch         ###   ########.fr       */
+/*   Updated: 2025/04/11 14:00:28 by yukravch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,26 @@
 
 int	main()
 {
-	char *str;
-
+	int fd[2];
+	pipe(fd);
+	int id = fork();
+	char *args1[] = {"/bin/ls", NULL};
+	char *args2[] = {"/bin/grep", "test",  NULL};
 	
-	str = malloc(sizeof(char) * ft_strlen(strerror(1)));
-	str = strerror(2);
-	ft_printf("%s", str);
+	if (id == 0)
+	{
+		close(fd[1]);
+		ft_printf("I'm a child process and I read from fd[0]\n");
+		dup2(fd[0], 0);
+		execve(args2[0], args2, NULL);
+		close(fd[0]);
+	}
+	else
+	{
+		close(fd[0]);
+		ft_printf("parent process and I write in fd[1]\n");
+		dup2(fd[1], 1);
+		close(fd[1]);
+		execve(args1[0], args1, NULL);
+	}
 }
