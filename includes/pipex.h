4d8feecf79287123bcfb51cb3ee01be05dbd0777
Yuliia Kravchenko <yukravch@paul-f4Br2s1.clusters.42paris.fr>
2025-04-13 20:52:47 +0200

    created 2 child processes with fork()
    first child will read from file1 executing the command1
    when it's finished
    second child will write the result of the execution of command2 on file2

--- includes/pipex.h
+++ includes/pipex.h
@@ -6,21 +6,23 @@
 /*   By: yukravch <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/09 19:19:12 by yukravch          #+#    #+#             */
-/*   Updated: 2025/04/11 17:45:28 by yukravch         ###   ########.fr       */
+/*   Updated: 2025/04/13 19:59:30 by yukravch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PIPEX_H
 # define PIPEX_H
 
-#include "libft.h"
-#include "ft_printf.h"
-#include "get_next_line.h"
+# include "libft.h"
+# include "ft_printf.h"
+# include "get_next_line.h"
 
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <fcntl.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <fcntl.h>
+# include <sys/types.h>
+# include <sys/wait.h>
 
 void	ft_exit(int fd1, int fd2);
 
--- sources/main.c
+++ sources/main.c
@@ -6,7 +6,7 @@
 /*   By: yukravch <yukravch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/09 19:18:14 by yukravch          #+#    #+#             */
-/*   Updated: 2025/04/13 19:16:32 by yukravch         ###   ########.fr       */
+/*   Updated: 2025/04/13 20:28:58 by yukravch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,18 @@ void	ft_check_file1_exists(char *file1)
 		exit(EXIT_FAILURE);
 	}
 }
+/*
+void	ft_read_from_file1(char *file1, char *cmd1)
+{
+	int	fd[2];
 
+	pipe(fd);
+	
+	fd[0] - read from it
+	fd[1] - write on it
+
+}
+*/
 int	main(int ac, char **av, char **envp)
 {
 	(void)envp;
@@ -36,11 +47,48 @@ int	main(int ac, char **av, char **envp)
 		exit(EXIT_FAILURE);
 	}
 	ft_check_file1_exists(av[1]);
-	int fd = open(av[4], O_WRONLY | O_CREAT, 00700);
-	if (fd == -1)
+	
+	int pid1 = fork();
+	if (pid1 == -1)
+	{
+		perror("pipex");
+		exit(EXIT_FAILURE);
+	}
+	if (pid1 == 0)
+	{
+		ft_printf("I'm first child, I read from file1\n");
+		//ft_read_from_file1(av[1], av[2]);
+		exit(EXIT_SUCCESS);
+	}
+	
+	waitpid(pid1, NULL, 0);
+	ft_printf("I'm parent: First child (pid %d) finished\n\n", pid1);
+
+	int	pid2 = fork();
+	if (pid2 == -1)
+	{
+		perror("pipex");
+		exit(EXIT_FAILURE);
+	}
+	if (pid2 == 0)
+	{
+		ft_printf("I'm second child, I write on file2\n");
+		exit(EXIT_SUCCESS);
+	}
+
+	waitpid(pid2, NULL, 0);
+	ft_printf("I'm parent: Second child (pid %d) finished\n", pid2);
+
+
+
+
+
+
+	int file2 = open(av[4], O_WRONLY | O_CREAT, 00700);
+	if (file2 == -1)
 	{
 		ft_printf("Fail");
 		exit(EXIT_FAILURE);
 	}
-	close(fd);
+	close(file2);
 }
